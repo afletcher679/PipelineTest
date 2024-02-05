06aacb4465e2fb96d04df156572444281c4b22b5
Ashley <afletcher679@gmail.com>
2024-02-05 15:52:51 -0500

    Copied files from azure

---
+++ PipelineWpfApp/PythonTests/test_app_opens.py
@@ -0,0 +1,73 @@
+import unittest
+import subprocess
+import pyautogui
+import pygetwindow
+import time
+import os
+
+process = None
+windows = []
+parent_dir = os.path.dirname(os.getcwd())
+testing_images_dir = os.path.join(os.getcwd(), 'PipelineWpfApp', 'wpfApp_screenshots')
+local_images_dir = os.path.join(parent_dir, 'wpfApp_screenshots')
+
+
+class TestAppUi(unittest.TestCase):
+    @staticmethod
+    def start_app():
+        global process
+        process = subprocess.Popen('C:\\Users\\tester\\Desktop\\PipelineTest\\drop\\PipelineWpfApp.exe')
+        time.sleep(3)
+        global windows
+        windows = pygetwindow.getWindowsWithTitle("Greetings")
+        if len(windows) == 1:
+            return True
+
+    def test_app_opens(self):
+        window_open = self.start_app()
+        self.assertTrue(window_open)
+
+    @staticmethod
+    def move_window_to_start_position():
+        window = windows[0]
+        window.moveTo(100, 100)
+
+    def find_image_90_confidence(self, image_path):
+        try:
+            x, y = pyautogui.locateCenterOnScreen(image_path, confidence=0.9)
+            return x, y
+        except pyautogui.ImageNotFoundException:
+            print(f'{image_path} could not be found with 90% confidence.')
+            self.fail(f'{image_path} could not be found with 90% confidence.')
+
+    def get_display_button_position(self):
+        display_button_image_path = os.path.join(testing_images_dir, 'display_button.PNG')
+        x, y = self.find_image_90_confidence(display_button_image_path)
+        return x, y
+
+    def get_goodbye_button_position(self):
+        goodbye_image_path = os.path.join(testing_images_dir, 'goodbye_unselected.PNG')
+        x, y = self.find_image_90_confidence(goodbye_image_path)
+        return x, y
+
+    @staticmethod
+    def get_screen_res():
+        print(pyautogui.size())
+
+    def test_select_goodbye_see_message(self):
+        self.get_screen_res()
+        self.move_window_to_start_position()
+
+        goodbye_select_position = self.get_goodbye_button_position()
+        pyautogui.click(goodbye_select_position[0], goodbye_select_position[1])
+
+        display_button_position = self.get_display_button_position()
+        pyautogui.click(display_button_position[0], display_button_position[1])
+
+        goodbye_image_path = os.path.join(testing_images_dir, 'goodbye_message.PNG')
+        result = pyautogui.locateOnWindow(goodbye_image_path, 'Greetings', confidence=0.9)
+        self.assertIsNotNone(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
