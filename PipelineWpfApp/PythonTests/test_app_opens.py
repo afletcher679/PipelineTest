e9bdd8802d176a8b82f46883f0f83996b0d3fd6d
Ashley <afletcher679@gmail.com>
2024-02-05 23:45:11 -0500

    made test with selecting good bye open app.

--- PipelineWpfApp/PythonTests/test_app_opens.py
+++ PipelineWpfApp/PythonTests/test_app_opens.py
@@ -5,7 +5,6 @@ import pygetwindow
 import time
 import os
 
-process = None
 windows = []
 parent_dir = os.path.dirname(os.getcwd())
 testing_images_dir = os.path.join(os.getcwd(), 'PipelineWpfApp', 'wpfApp_screenshots')
@@ -13,14 +12,21 @@ local_images_dir = os.path.join(parent_dir, 'wpfApp_screenshots')
 
 
 class TestAppUi(unittest.TestCase):
+
     @staticmethod
     def start_app():
-        global process
-        process = subprocess.Popen('C:\\Users\\tester\\Desktop\\PipelineTest\\drop\\PipelineWpfApp.exe')
+        subprocess.Popen('C:\\Users\\tester\\Desktop\\PipelineTest\\drop\\PipelineWpfApp.exe')
         time.sleep(3)
         global windows
         windows = pygetwindow.getWindowsWithTitle("Greetings")
-        if len(windows) == 1:
+        matching_windows = len(windows)
+        if matching_windows == 1:
+            return True
+        elif matching_windows > 1:
+            while matching_windows != 1:
+                windows[matching_windows - 1].close()
+                windows = pygetwindow.getWindowsWithTitle("Greetings")
+                matching_windows = len(windows)
             return True
 
     def test_app_opens(self):
@@ -55,7 +61,7 @@ class TestAppUi(unittest.TestCase):
         print(pyautogui.size())
 
     def test_select_goodbye_see_message(self):
-        self.get_screen_res()
+        self.test_app_opens()
         self.move_window_to_start_position()
 
         goodbye_select_position = self.get_goodbye_button_position()
